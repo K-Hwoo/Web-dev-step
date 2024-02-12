86f4c21093d24496661b5d2f84bb250ad31ffcc5
Hyunwoo Kim <aq3480@naver.com>
2024-02-12 16:06:28 +0900

    completed seventh project

--- seventh project/scripts/app.js
+++ seventh project/scripts/app.js
@@ -8,6 +8,10 @@ const backdrop = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
 
 let editedPlayer = 0;
+let activePlayer = 0;
+let currentRound = 1;
+let gameIsOver = false;
+
 const players = [
   {
     name: "",
@@ -19,6 +23,19 @@ const players = [
   },
 ];
 
+const startNewGameBtn = document.getElementById("start-game-btn");
+const gameAreaElement = document.getElementById("active-game");
+const gameFieldElements = document.querySelectorAll("#game-board li");
+const activePlayerNameElement = document.getElementById("active-player-name");
+
+const gameData = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
+
+const gameOverElement = document.getElementById("game-over");
+
 editPlayer1Btn.addEventListener("click", openEditWindow); // 함수는 config.js에
 editPlayer2Btn.addEventListener("click", openEditWindow);
 
@@ -26,3 +43,13 @@ cancleBtn.addEventListener("click", closeEditWindow);
 backdrop.addEventListener("click", closeEditWindow);
 
 formElement.addEventListener("submit", savePlayerConfig);
+
+startNewGameBtn.addEventListener("click", startNewGame);
+for (const gameFieldElement of gameFieldElements) {
+  gameFieldElement.addEventListener("click", selectGameField);
+}
+
+// const gameBoardElement = document.getElementById("game-board");
+// gameBoardElement.addEventListener("click", selectGameField);
+// 이렇게 해도 li들을 클릭했을 때 함수가 작동
+// ol에서 event target에 대한 값으로 클릭한 가장 구체적인 마지막 항목을 가져옴
---
+++ seventh project/scripts/game.js
@@ -0,0 +1,132 @@
+function resetGameStatus() {
+  activePlayer = 0;
+  currentRound = 1;
+  gameIsOver = false;
+  gameOverElement.firstElementChild.innerHTML =
+    'You won, <span id="winner-name">PLAYER NAME</span> !';
+  gameOverElement.style.display = "none";
+
+  let gameBoardIndex = 0;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      gameData[i][j] = 0;
+      gameFieldElements[gameBoardIndex].textContent = "";
+      gameFieldElements[gameBoardIndex].classList.remove("disabled");
+      gameBoardIndex++;
+    }
+  }
+}
+
+function startNewGame() {
+  if (players[0].name && players[1].name) {
+    resetGameStatus();
+
+    activePlayerNameElement.textContent = players[activePlayer].name;
+    gameAreaElement.style.display = "block";
+  } else {
+    alert("Please set custom player names for both players!");
+    return;
+  }
+}
+
+function checkForGameOver() {
+  // Checking the rows for equality
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
+    }
+  }
+
+  // Checking the columns for equality
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[1][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
+    }
+  }
+
+  // Diagonal: Top left to bottom right
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
+
+  // Diagonal: Top right to bottom left
+  if (
+    gameData[0][2] > 0 &&
+    gameData[0][2] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][0]
+  ) {
+    return gameData[0][2];
+  }
+
+  if (currentRound === 9) {
+    return -1; // Draw
+  }
+
+  return 0;
+}
+
+function endGame(winnerId) {
+  gameIsOver = true;
+  gameOverElement.style.display = "block";
+
+  if (winnerId > 0) {
+    gameOverElement.firstElementChild.firstElementChild.textContent =
+      players[winnerId - 1].name;
+  } else {
+    gameOverElement.firstElementChild.textContent = "It's a draw !";
+  }
+}
+
+function switchPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+function selectGameField(event) {
+  const selectedField = event.target;
+  //   if (selectedField.tagName !== "LI") {
+  //     return;
+  //   }
+  if (gameIsOver) {
+    return;
+  }
+
+  const selectedRow = selectedField.dataset.row - 1;
+  const selectedColumn = selectedField.dataset.col - 1;
+
+  if (gameData[selectedRow][selectedColumn] > 0) {
+    alert("Please select an empty field!");
+    return;
+  }
+
+  selectedField.textContent = players[activePlayer].symbol;
+  selectedField.classList.add("disabled");
+
+  gameData[selectedRow][selectedColumn] = activePlayer + 1;
+  // console.log(gameData);
+
+  const winnerId = checkForGameOver();
+  // console.log(winnerId);
+  if (winnerId !== 0) {
+    endGame(winnerId);
+  }
+
+  currentRound++;
+  switchPlayer();
+}
