e0b24bbae33c20b59f3940328cc3353f541e33cf
Hyunwoo Kim <aq3480@naver.com>
2024-04-05 10:28:21 +0900

    authentication

---
+++ sixteenth project/app.js
@@ -0,0 +1,48 @@
+const path = require("path");
+
+const express = require("express");
+const session = require("express-session");
+const mongodbStore = require("connect-mongodb-session");
+
+const db = require("./data/database");
+const demoRoutes = require("./routes/demo");
+
+const mongoDBStore = mongodbStore(session);
+
+const app = express();
+
+const sessionStore = new mongoDBStore({
+  uri: "mongodb://localhost:27017",
+  databaseName: "auth-demo",
+  collection: "sessions",
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: "super-secret",
+    resave: false,
+    saveUninitialized: false,
+    // 내부에 데이터가 없는 빈 세션은 데이터베이스에 저장되지 않으므로 세션 쿠키 생성 X
+    store: sessionStore, // 세션 데이터가 실제로 저장되어야 하는 위치 제어
+    cookie: {
+      maxAge: 60 * 1000 * 60 * 24 * 30, // 1초   60분   24시간   30일
+      // 30일 뒤 세션 만료
+    },
+  })
+);
+
+app.use(demoRoutes);
+
+app.use(function (error, req, res, next) {
+  res.render("500");
+});
+
+db.connectToDatabase().then(function () {
+  app.listen(3000);
+});
---
+++ sixteenth project/routes/demo.js
@@ -0,0 +1,138 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+
+const db = require("../data/database");
+
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  res.render("welcome");
+});
+
+router.get("/signup", function (req, res) {
+  let sessionInputData = req.session.inputData;
+
+  if (!sessionInputData) {
+    sessionInputData = {
+      hasError: false,
+      email: "",
+      confirmEmail: "",
+      password: "",
+    };
+  }
+
+  req.session.inputData = null;
+
+  res.render("signup", { inputData: sessionInputData });
+});
+
+router.get("/login", function (req, res) {
+  res.render("login");
+});
+
+router.post("/signup", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email; // userData["email"];
+  const enteredConfirmEmail = userData["confirm-email"]; // .표기법에서 dash는 사용불가
+  const enteredPassword = userData.password;
+
+  if (
+    !enteredEmail ||
+    !enteredConfirmEmail ||
+    !enteredPassword ||
+    enteredPassword.trim() < 6 ||
+    enteredEmail !== enteredConfirmEmail ||
+    !enteredEmail.includes("@")
+  ) {
+    req.session.inputData = {
+      hasError: true,
+      message: "Invalid input - please check yout data.",
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword,
+    };
+
+    req.session.save(function () {
+      console.log("Incorrect data");
+      return res.redirect("/signup");
+    });
+
+    return; // 서버 충돌 방지
+  } // 회원 가입시 에러 처리
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+
+  if (existingUser) {
+    return res.redirect("/signup");
+  }
+
+  const hashedPassword = await bcrypt.hash(enteredPassword, 12);
+
+  const user = {
+    email: enteredEmail,
+    password: hashedPassword,
+  };
+
+  await db.getDb().collection("users").insertOne(user);
+
+  res.redirect("/login");
+});
+
+router.post("/login", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email; // userData["email"];
+  const enteredPassword = userData.password;
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+
+  if (!existingUser) {
+    console.log("Could not log in!");
+    return res.redirect("/login");
+  }
+
+  const passwordAreEqual = await bcrypt.compare(
+    enteredPassword,
+    existingUser.password
+  );
+
+  if (!passwordAreEqual) {
+    console.log("Could not log in! - password are not correct!");
+    return res.redirect("/login");
+  }
+
+  req.session.user = {
+    id: existingUser._id,
+    email: existingUser.email,
+  };
+  req.session.isAuthenticated = true;
+  req.session.save(function () {
+    // 인증이 필요한 페이지로의 리다이렉트를 위해
+    console.log("User is authenticated!");
+    res.redirect("/admin");
+  });
+});
+
+router.get("/admin", async function (req, res) {
+  // Check the user "ticket"
+  console.log(req.session);
+  if (!req.session.isAuthenticated) {
+    // if (!req.session.user) -> req.session.isAuthenticated 설정 안했을 시
+    // return res.status(401).render("401");
+    return res.redirect("/login"); // -> 비로그인 시 로그인 페이지로 리다이렉트
+  }
+  res.render("admin");
+});
+
+router.post("/logout", async function (req, res) {
+  req.session.user = null;
+  req.session.isAuthenticated = false;
+  res.redirect("/");
+});
+
+module.exports = router;
